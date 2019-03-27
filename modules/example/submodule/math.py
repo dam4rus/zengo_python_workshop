f91d8c675a140afe57945e85e88fa7ee0ebe27b5
Kalmár Róbert <rfrostkalmar@gmail.com>
2019-03-27 19:18:23 +0100

    Modules óra anyaga

---
+++ modules/example/submodule/math.py
@@ -0,0 +1,5 @@
+from .. import fibonacci
+
+
+def fib_to_100():
+    return fibonacci.fib(100)
---
+++ modules/importexamples.py
@@ -0,0 +1,48 @@
+# import fibonacci
+#
+# if __name__ == '__main__':
+#     print(fibonacci.__name__)
+#     print(fibonacci.fib(100))
+
+
+# from fibonacci import fib
+#
+# if __name__ == '__main__':
+#     print(fib(100))
+
+
+# import fibonacci as fib
+#
+# if __name__ == '__main__':
+#     print(fib.__name__)
+#     print(fib.fib(100))
+
+
+# from fibonacci import fib as fibo
+#
+# if __name__ == '__main__':
+#     print(fibo(100))
+
+
+# import fibonacci
+#
+# if __name__ == '__main__':
+#     print(dir(fibonacci))
+
+
+# import example.fibonacci
+#
+# if __name__ == '__main__':
+#     print(example.fibonacci.fib(100))
+
+
+# import example
+#
+# if __name__ == '__main__':
+#     print(example.fib(100))
+
+
+# import example.submodule
+#
+# if __name__ == '__main__':
+#     print(example.submodule.fib_to_100())
